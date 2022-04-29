b75be00d72003d759a4f45da757fde6e37021e43
Yash-Gemini <Yash.A.Gupta@Geminisolutions.com>
2022-04-29 15:37:48 +0530

    first

---
+++ src/app/yash/yash.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { YashComponent } from './yash.component';
+
+describe('YashComponent', () => {
+  let component: YashComponent;
+  let fixture: ComponentFixture<YashComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ YashComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(YashComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/yash/yash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yash',
+  templateUrl: './yash.component.html',
+  styleUrls: ['./yash.component.css']
+})
+export class YashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
